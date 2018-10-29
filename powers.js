fbc1386e0971f6a8feccc2a807c2b73671e3d917
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-10-29 15:54:43 -0500

    combined activate and deactivate functions into one

--- powers.js
+++ powers.js
@@ -2,27 +2,33 @@
 
 document.querySelector("#activate-flight").addEventListener("click", (event) => {
   let currentPowerId = event.target.id.split("-")[1]
- activatePower(currentPowerId)
+  activatePower(currentPowerId)
 })
 
 document.querySelector("#activate-mindreading").addEventListener("click", (event) => {
-  mindreadingHandlerFunction();
+  let currentPowerId = event.target.id.split("-")[1]
+  activatePower(currentPowerId)
 })
 
 document.querySelector("#activate-xray").addEventListener("click", (event) => {
-  xrayHandlerFunction();
-})
+  let currentPowerId = event.target.id.split("-")[1]
+  activatePower(currentPowerId)})
 
 document.querySelector("#activate-all").addEventListener("click", (event) => {
-  activateAllPowers();
-})
+  allPowersHandler();
+}
+)
 
 document.querySelector("#deactivate-all").addEventListener("click", (event) => {
-  deactivateAllPowers();
-})
+  allPowersHandler();
+  }
+)
 
 
 // event handler functions
+
+const powers = document.querySelectorAll(".power")
+
 
 const activatePower = (sectionId) => {
   let currentPowerSection = document.getElementById(sectionId)
@@ -30,40 +36,17 @@ const activatePower = (sectionId) => {
   currentPowerSection.classList.add("enabled");
 }
 
-const flightHandlerFunction = () => {
- const flightSection = document.querySelector("#flight")
- flightSection.classList.remove("disabled");
- flightSection.classList.add("enabled")
-}
-
-const mindreadingHandlerFunction = () => {
-  const mindreadingSection = document.querySelector("#mindreading")
-  mindreadingSection.classList.remove("disabled");
-  mindreadingSection.classList.add("enabled");
-}
-
-
-const xrayHandlerFunction = () => {
-  const xraySection = document.querySelector("#xray")
-  xraySection.classList.remove("disabled");
-  xraySection.classList.add("enabled");
-}
-
-const powers = document.querySelectorAll(".power")
-
-const activateAllPowers = () => {
+const allPowersHandler = () => {
   for (let i = 0; i < powers.length; i++) {
     let currentPower = powers[i];
-    currentPower.classList.remove("disabled")
-    currentPower.classList.add("enabled")
+    if (currentPower.classList.contains("disabled")) {
+      currentPower.classList.remove("disabled")
+      currentPower.classList.add("enabled")
+    } else if (currentPower.classList.contains("enabled")) {
+      currentPower.classList.remove("enabled")
+      currentPower.classList.add("disabled")
+    }
   }
 }
 
-const deactivateAllPowers = () => {
-  for (let i = 0; i < powers.length; i++) {
-    let currentPower = powers[i];
-    currentPower.classList.remove("enabled")
-    currentPower.classList.add("disabled")
-  }
-}
 
